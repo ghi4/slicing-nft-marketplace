dd60903c8e887e1753e2e9ed3273a6e1eff88ee6
Dhimas Fajarw <ghidet4pcoop@gmail.com>
2022-09-21 12:33:18 +0700

    Done, but container need to blur

--- lib/widgets/live_bids_card.dart
+++ lib/widgets/live_bids_card.dart
@@ -6,22 +6,25 @@ class LiveBidsCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Decoration boxDecoration = BoxDecoration(
+      color: Colors.white.withOpacity(0.6),
+      borderRadius: BorderRadius.circular(12),
+      border: Border.all(
+        color: Colors.white,
+        width: 1,
+      ),
+    );
+
     Widget liveCounter() {
       return Container(
-        width: 204,
+        width: 206,
         height: 36,
         margin: const EdgeInsets.only(top: 16),
         padding: const EdgeInsets.symmetric(
           horizontal: 8,
           vertical: 6,
         ),
-        decoration: BoxDecoration(
-            color: Colors.white.withOpacity(0.6),
-            borderRadius: BorderRadius.circular(12),
-            border: Border.all(
-              color: Colors.white,
-              width: 1,
-            )),
+        decoration: boxDecoration,
         child: Row(
           children: [
             Image.asset(
@@ -50,6 +53,102 @@ class LiveBidsCard extends StatelessWidget {
       );
     }
 
+    Widget nftInfo() {
+      return Container(
+        width: 248,
+        // height: 57,
+        margin: const EdgeInsets.only(
+          bottom: 15,
+          left: 16,
+          right: 16,
+        ),
+        padding: const EdgeInsets.symmetric(
+          horizontal: 12,
+          vertical: 11,
+        ),
+        decoration: boxDecoration,
+        child: Row(
+          children: [
+            // Profile Photo
+            Container(
+              width: 35,
+              height: 35,
+              decoration: const BoxDecoration(
+                shape: BoxShape.circle,
+                image: DecorationImage(
+                  fit: BoxFit.fill,
+                  image: AssetImage('assets/images/profile_1.png'),
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 4,
+            ),
+
+            // NFT Name and Creator Name
+            Expanded(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Text(
+                    'Astroboy #001',
+                    style: primaryTextStyle.copyWith(
+                      fontSize: 12,
+                      fontWeight: medium,
+                    ),
+                  ),
+                  Row(
+                    children: [
+                      Text(
+                        'By Bryan Wan',
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 10,
+                        ),
+                      ),
+                      const SizedBox(
+                        width: 2,
+                      ),
+                      Image.asset(
+                        'assets/images/badge_checklist_blue.png',
+                        width: 12,
+                      ),
+                    ],
+                  ),
+                ],
+              ),
+            ),
+
+            // Highest Bid
+            Column(
+              children: [
+                Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/ic_ethereum.png',
+                      width: 9,
+                    ),
+                    Text(
+                      '2.70 ETH',
+                      style: primaryTextStyle.copyWith(
+                        fontSize: 12,
+                        fontWeight: medium,
+                      ),
+                    ),
+                  ],
+                ),
+                Text(
+                  'Highest Bid',
+                  style: primaryTextStyle.copyWith(
+                    fontSize: 10,
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
+      );
+    }
+
     return Container(
       width: 280,
       height: 400,
@@ -64,7 +163,7 @@ class LiveBidsCard extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
           liveCounter(),
-          liveCounter(),
+          nftInfo(),
         ],
       ),
     );
