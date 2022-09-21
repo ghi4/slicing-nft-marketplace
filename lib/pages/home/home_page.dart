e4f59f3b7bea04a95d177d2684ee7305f1debbba
Dhimas Fajarw <ghidet4pcoop@gmail.com>
2022-09-21 17:14:31 +0700

    Move views to home_page

--- lib/pages/home/home_page.dart
+++ lib/pages/home/home_page.dart
@@ -1,12 +1,136 @@
 import 'package:flutter/material.dart';
+import 'package:nft_marketplace/theme.dart';
+import 'package:nft_marketplace/widgets/creator_card.dart';
+import 'package:nft_marketplace/widgets/live_bids_card.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Home Page'),
+    appBar() {
+      return Container(
+        margin: const EdgeInsets.symmetric(horizontal: 20),
+        height: 70,
+        child: Row(
+          children: [
+            Expanded(
+              child: Container(
+                height: 46,
+                padding: const EdgeInsets.symmetric(horizontal: 16),
+                decoration: BoxDecoration(
+                  color: backgroundColor2,
+                  borderRadius: BorderRadius.circular(12),
+                ),
+                child: Row(
+                  children: [
+                    Image.asset(
+                      'assets/images/ic_search.png',
+                      width: 24,
+                    ),
+                    const SizedBox(
+                      width: 4,
+                    ),
+                    Expanded(
+                      child: TextFormField(
+                        style: primaryTextStyle.copyWith(
+                          fontSize: 12,
+                        ),
+                        decoration: InputDecoration.collapsed(
+                          hintText: 'Search NFT',
+                          hintStyle: subtitleTextStyle2,
+                        ),
+                      ),
+                    ),
+                    Image.asset(
+                      'assets/images/ic_mic.png',
+                      width: 24,
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            const SizedBox(
+              width: 18,
+            ),
+            Image.asset(
+              'assets/images/ic_notification_dot.png',
+              width: 46,
+            ),
+          ],
+        ),
+      );
+    }
+
+    Widget sectionTitle(String titleName) {
+      return Container(
+        margin: const EdgeInsets.only(
+          top: 20,
+          bottom: 12,
+          left: 20,
+          right: 20,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              titleName,
+              style: primaryTextStyle.copyWith(
+                fontSize: 20,
+                fontWeight: medium,
+              ),
+            ),
+            Text(
+              'See More',
+              style: subtitleTextStyle,
+            )
+          ],
+        ),
+      );
+    }
+
+    Widget contentLiveBids() {
+      return Container(
+        height: 400,
+        padding: const EdgeInsets.only(left: 20),
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          shrinkWrap: true,
+          clipBehavior: Clip.none,
+          children: const [
+            LiveBidsCard(),
+            LiveBidsCard(),
+            LiveBidsCard(),
+          ],
+        ),
+      );
+    }
+
+    Widget contentTopCreator() {
+      return Container(
+        height: 150,
+        margin: const EdgeInsets.only(bottom: 32),
+        padding: const EdgeInsets.only(left: 20),
+        child: ListView(
+          scrollDirection: Axis.horizontal,
+          children: const [
+            CreatorCard(),
+            CreatorCard(),
+            CreatorCard(),
+          ],
+        ),
+      );
+    }
+
+    return ListView(
+      shrinkWrap: true,
+      children: [
+        appBar(),
+        sectionTitle('Live Bids'),
+        contentLiveBids(),
+        sectionTitle('Top Creator'),
+        contentTopCreator(),
+      ],
     );
   }
 }
--- lib/pages/home/navigation_page.dart
+++ lib/pages/home/navigation_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:nft_marketplace/pages/home/home_page.dart';
 import 'package:nft_marketplace/theme.dart';
 import 'package:nft_marketplace/widgets/creator_card.dart';
 import 'package:nft_marketplace/widgets/live_bids_card.dart';
@@ -15,87 +16,6 @@ class _NavigationPageState extends State<NavigationPage> {
 
   @override
   Widget build(BuildContext context) {
-    appBar() {
-      return Container(
-        margin: const EdgeInsets.symmetric(horizontal: 20),
-        height: 70,
-        child: Row(
-          children: [
-            Expanded(
-              child: Container(
-                height: 46,
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                decoration: BoxDecoration(
-                  color: backgroundColor2,
-                  borderRadius: BorderRadius.circular(12),
-                ),
-                child: Row(
-                  children: [
-                    Image.asset(
-                      'assets/images/ic_search.png',
-                      width: 24,
-                    ),
-                    const SizedBox(
-                      width: 4,
-                    ),
-                    Expanded(
-                      child: TextFormField(
-                        style: primaryTextStyle.copyWith(
-                          fontSize: 12,
-                        ),
-                        decoration: InputDecoration.collapsed(
-                          hintText: 'Search NFT',
-                          hintStyle: subtitleTextStyle2,
-                        ),
-                      ),
-                    ),
-                    Image.asset(
-                      'assets/images/ic_mic.png',
-                      width: 24,
-                    ),
-                  ],
-                ),
-              ),
-            ),
-            const SizedBox(
-              width: 18,
-            ),
-            Image.asset(
-              'assets/images/ic_notification_dot.png',
-              width: 46,
-            ),
-          ],
-        ),
-      );
-    }
-
-    Widget sectionTitle(String titleName) {
-      return Container(
-        margin: const EdgeInsets.only(
-          top: 20,
-          bottom: 12,
-          left: 20,
-          right: 20,
-        ),
-        child: Row(
-          mainAxisAlignment: MainAxisAlignment.spaceBetween,
-          children: [
-            Text(
-              titleName,
-              style: primaryTextStyle.copyWith(
-                fontSize: 20,
-                fontWeight: medium,
-              ),
-            ),
-            Text(
-              'See More',
-              style: subtitleTextStyle,
-            )
-          ],
-        ),
-      );
-    }
-
     Widget bottomNavigationBar() {
       return BottomAppBar(
         child: BottomNavigationBar(
@@ -144,52 +64,10 @@ class _NavigationPageState extends State<NavigationPage> {
         ),
       );
     }
-
-    Widget contentLiveBids() {
-      return Container(
-        height: 400,
-        padding: const EdgeInsets.only(left: 20),
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          shrinkWrap: true,
-          clipBehavior: Clip.none,
-          children: const [
-            LiveBidsCard(),
-            LiveBidsCard(),
-            LiveBidsCard(),
-          ],
-        ),
-      );
-    }
-
-    Widget contentTopCreator() {
-      return Container(
-        height: 150,
-        margin: const EdgeInsets.only(bottom: 32),
-        padding: const EdgeInsets.only(left: 20),
-        child: ListView(
-          scrollDirection: Axis.horizontal,
-          children: const [
-            CreatorCard(),
-            CreatorCard(),
-            CreatorCard(),
-          ],
-        ),
-      );
-    }
-
     return SafeArea(
       child: Scaffold(
         backgroundColor: backgroundColor1,
-        body: ListView(
-          children: [
-            appBar(),
-            sectionTitle('Live Bids'),
-            contentLiveBids(),
-            sectionTitle('Top Creator'),
-            contentTopCreator(),
-          ],
-        ),
+        body: const HomePage(),
         bottomNavigationBar: bottomNavigationBar(),
       ),
     );
